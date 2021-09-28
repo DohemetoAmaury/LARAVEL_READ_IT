07f20fce612326b38ff80b29dc2d29772812bb23
Dohemeto Amaury <dohemetoamaury@gmail.com>
2021-09-28 09:55:29 +0200

    décomposition en partials

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('template.index');
 });
+
+Route::get('/posts', [App\Http\Controllers\PostsController::class, 'index']);
