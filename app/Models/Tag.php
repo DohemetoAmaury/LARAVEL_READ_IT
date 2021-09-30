bbc9920cee8d3f52ddfffc93f836343e37c92fc8
Dohemeto Amaury <dohemetoamaury@gmail.com>
2021-09-30 09:13:12 +0200

    mise en place des tags avec BelongsToMany

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Tag extends Model
 {
-  public function tags() {
-      return $this->belongsToMany(Tag::class, 'posts_has_tags');
-  }
+    public function posts() {
+      return $this->belongsToMany(Post::class,'posts_has_tags');
+    }
 }
