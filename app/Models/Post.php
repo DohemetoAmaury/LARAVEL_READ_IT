78096274ee03448e52cf21b11751e758fe89478c
Dohemeto Amaury <dohemetoamaury@gmail.com>
2021-10-05 11:29:44 +0200

    Mise en place des commentaires, side bar et nombre de categories

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,10 @@ class Post extends Model
   public function author(){
       return $this->belongsTo(Author::class);
         }
+  public function comments(){
+      return $this->hasMany(Comment::class);
+        }
+  public function categorie(){
+      return $this->belongsTo(Categorie::class, 'category_id');
+        }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -20,59 +20,8 @@
 
  @include('tags._index', ['tags' => $post->tags])
  @include('authors._show', ['author' => $post->authors])
-
-
-
-
-<div class="pt-5 mt-5">
-  <h3 class="mb-5">3 Comments</h3>
-  <ul class="comment-list">
-    <li class="comment">
-      <div class="comment-body">
-        <h3>John Doe</h3>
-        <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-      </div>
-    </li>
-
-    <li class="comment">
-      <div class="comment-body">
-        <h3>John Doe</h3>
-        <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-      </div>
-    </li>
-
-    <li class="comment">
-      <div class="comment-body">
-        <h3>John Doe</h3>
-        <div class="meta mb-3">November 13, 2019 at 2:21pm</div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-      </div>
-    </li>
-  </ul>
-  <!-- END comment-list -->
-
-  <div class="comment-form-wrap pt-5">
-    <h3 class="mb-5">Leave a comment</h3>
-    <form action="#" class="p-5 bg-light" method="post">
-      <div class="form-group">
-        <label for="name">Name *</label>
-        <input type="text" class="form-control" id="name">
-      </div>
-      <div class="form-group">
-        <label for="message">Message</label>
-        <textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
-      </div>
-      <div class="form-group">
-        <input type="hidden" name="postId" value="4" />
-        <input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
-      </div>
-
-    </form>
-  </div>
-</div>
-
+ @include('comments._index', ['comments' => $post->comments])
+ 
 </div> <!-- .col-md-8 -->
 <div class="col-lg-4 sidebar pl-lg-5 ftco-animate">
 <div class="sidebar-box">
